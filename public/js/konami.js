80e93c0f8deef661fb494ae4ba1fb69a5c2e9407
Ben Roberts <mikebenroberts@gmail.com>
2017-03-27 17:17:25 -0500

    allowed for easter egg to play multiple times

--- public/js/konami.js
+++ public/js/konami.js
@@ -1,6 +1,6 @@
 (function() {
 	'use strict';
-
+function konami(){
 	var  keyArray = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
 
 	$(document).keyup(function(e) {
@@ -15,14 +15,16 @@
 				$("#joey")[0].src += "&autoplay=1";
 				setTimeout(function(){
 					$("#joey").hide();
+					$("#joey")[0].src.slice(0,-11);
 				}, 10000);
-
+				konami();
 			};
 
 		} else {
 
 			keyArray = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
-
 		};
 	});
+}
+konami();
 })();
